e7c7e1136ef88c3deab84c850281befce441c202
Deepakhannan <deepakhannan.lakshmipathi@trumpf.com>
2025-09-17 16:38:00 +0530

    T0: Dummy assignment

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -8,7 +8,7 @@
 namespace Training25 {
    internal class Program {
       static void Main (string[] args) {
-         Console.WriteLine ("Hello, World!");
+         Console.WriteLine ("First assignment");
       }
    }
 }
