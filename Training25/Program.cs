d7f43bce362248514fce089db666d4d5c0e65554
Deepakhannan <deepakhannan.lakshmipathi@trumpf.com>
2025-09-22 15:14:18 +0530

    Second commit

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -8,54 +8,67 @@
 namespace Training25 {
    internal class Program {
       static void Main (string[] args) {
-         Console.WriteLine ("Enter the number that is to be converted:");
-         int input1;
-         int input2;
+         int input1, input2;
          bool isValid;
-         string numSystem = "";
-         string result = "";
+         string numSystem = "", result = "";
+         Console.WriteLine ("Enter the number that is to be converted:");
+
          do {
             isValid = int.TryParse (Console.ReadLine (), out input1);
             if (isValid == false) Console.WriteLine ("Please enter a valid number !");
-         }while (!isValid);
+         } while (!isValid);
          input2 = input1;
+
          Console.WriteLine ("Enter the number system to convert:\nb - binary\th-hexa");
+
          do {
-            numSystem = Console.ReadLine ();
-            if (numSystem.ToLower ().Trim () == "b") {
-               // Binary conversion
-               do {
-                  int remain = input2 % 2;
-                  input2 = input2 / 2;
-                  result = remain.ToString () + result;
-               }
-               while (input2 != 0);
-            } else if (numSystem.ToLower ().Trim () == "h") {
-               // Hexa conversion
-               do {
-                  int remain = input2 % 16;
-                  input2 = input2 / 16;
-                  string remainHexa = "";
-                  if (remain / 10 == 0) {
-                     result = remain.ToString () + result;
-                  } else {
-                     switch (remain) {
-                        case 10: remainHexa = "A"; break;
-                        case 11: remainHexa = "B"; break;
-                        case 12: remainHexa = "C"; break;
-                        case 13: remainHexa = "D"; break;
-                        case 14: remainHexa = "E"; break;
-                        case 15: remainHexa = "F"; break;
-                     }
-                     result = remainHexa + result;
-                  }
-               } while (input2 != 0);
+            numSystem = Console.ReadLine ().ToLower ().Trim ();
+            if (numSystem == "b") {
+               if (input2 == 0) result = "Zero";
+               Binary (input2);
+            } else if (numSystem == "h") {
+               if (input2 == 0) result = "0";
+               Hexa (input2);
             } else {
                Console.WriteLine ("Please enter a valid input !");
             }
-         }while (numSystem.ToLower ().Trim () != "b" && numSystem.ToLower ().Trim () != "h");
-         if (numSystem.ToLower ().Trim () == "b") Console.WriteLine ($"The converted binary value of {input1} is {result}");
-         else Console.WriteLine ($"The converted hexadecimal value of {input1} is {result}");
+         } while (numSystem != "b" && numSystem != "h");
+
+
+
+         void Hexa (int input2) {
+            do {
+               int remain = input2 % 16;
+               input2 /= 16;
+               string remainHexa = "";
+               if (remain / 10 == 0) {
+                  result = remain.ToString () + result;
+               } else {
+                  switch (remain) {
+                     case 10: remainHexa = "A"; break;
+                     case 11: remainHexa = "B"; break;
+                     case 12: remainHexa = "C"; break;
+                     case 13: remainHexa = "D"; break;
+                     case 14: remainHexa = "E"; break;
+                     case 15: remainHexa = "F"; break;
+                  }
+                  result = remainHexa + result;
+               }
+            } while (input2 != 0);
+            Console.WriteLine ($"The converted hexadecimal value of {input1} is {result}");
+         }
+         void Binary (int input2) {
+            do {
+               int remain = input2 % 2;
+               input2 /= 2;
+               result = remain.ToString () + result;
+            }
+            while (input2 != 0);
+            Console.WriteLine ($"The converted binary value of {input1} is {result}");
+         }
+
+
+
       }
    }
 }
