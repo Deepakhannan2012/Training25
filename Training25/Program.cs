8e0947cf12e1f174f2f3c3fe9b95ac81f45da6d3
Deepakhannan <deepakhannan.lakshmipathi@trumpf.com>
2025-09-22 14:52:11 +0530

    First commit

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -8,7 +8,29 @@
 namespace Training25 {
    internal class Program {
       static void Main (string[] args) {
-         Console.WriteLine ("Hello, World!");
+         int result = 0;
+         int myNum1 = int.Parse (Console.ReadLine ());
+         int myNum2 = int.Parse (Console.ReadLine ());
+
+
+         int LcmNum (int myNum1, int myNum2) {
+            for (int i = 1; i <= myNum2; i++) {
+               int temp1 = myNum1 * i;
+               for (int j = 1; j <= myNum1; j++) {
+                  int temp2 = myNum2 * j;
+                  if (temp1 == temp2) {
+                     return temp1;
+                  }
+               }
+            }
+            return myNum1 * myNum2;
+         }
+         int Gcd(int myNum1, int myNum2) {
+            return (myNum1 * myNum2) / LcmNum (myNum1, myNum2);
+         }
+         Console.WriteLine (LcmNum(myNum1,myNum2));
+         Console.WriteLine (Gcd(myNum1,myNum2));
+
       }
    }
 }
