92cd3562ea345a2f64e7e38d67cf29a93a06ea67
Deepakhannan <deepakhannan.lakshmipathi@trumpf.com>
2025-09-22 17:40:28 +0530

    Fourth commit

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -8,62 +8,58 @@
 namespace Training25 {
    internal class Program {
       static void Main (string[] args) {
-         int input1, input2;
-         bool isValid;
-         string numSystem = "", result = "";
          Console.WriteLine ("Enter the number that is to be converted:");
-
          do {
-            isValid = int.TryParse (Console.ReadLine (), out input1);
-            if (isValid == false) Console.WriteLine ("Please enter a valid number !");
-         } while (!isValid);
-         input2 = input1;
-
+            sIsValid = int.TryParse (Console.ReadLine (), out sInput1);
+            if (sIsValid == false) Console.WriteLine ("Please enter a valid number !");
+         } while (!sIsValid);
+         sInput2 = sInput1;
          Console.WriteLine ("Enter the number system to convert:\nb - binary\th-hexa");
-
          do {
-            numSystem = Console.ReadLine ().ToLower ().Trim ();
-            if (numSystem == "b") {
-               if (input2 == 0) result = "Zero";
-               Binary (input2);
-            } else if (numSystem == "h") {
-               if (input2 == 0) result = "0";
-               Hexa (input2);
-            } else {
-               Console.WriteLine ("Please enter a valid input !");
-            }
-         } while (numSystem != "b" && numSystem != "h");
+            sNumSystem = Console.ReadLine ().ToLower ().Trim ();
+            if (sNumSystem == "b") Binary (sInput2);
+            else if (sNumSystem == "h") Hexa (sInput2);
+            else Console.WriteLine ("Please enter a valid input !");
+         } while (sNumSystem != "b" && sNumSystem != "h");
+      }
 
-         void Hexa (int input2) {
-            do {
+      static void Hexa (int input2) {
+         do {
+            if (input2 == 0) sResult = "0";
+            else {
                int remain = input2 % 16;
                input2 /= 16;
-               string remainHexa = "";
-               if (remain / 10 == 0) {
-                  result = remain.ToString () + result;
-               } else {
-                  switch (remain) {
-                     case 10: remainHexa = "A"; break;
-                     case 11: remainHexa = "B"; break;
-                     case 12: remainHexa = "C"; break;
-                     case 13: remainHexa = "D"; break;
-                     case 14: remainHexa = "E"; break;
-                     case 15: remainHexa = "F"; break;
-                  }
-                  result = remainHexa + result;
+               if (remain / 10 == 0) sResult = remain.ToString () + sResult;
+               else {
+                  string remainHexa = remain switch {
+                     10 => "A",
+                     11 => "B",
+                     12 => "C",
+                     13 => "D",
+                     14 => "E",
+                     15 => "F"
+                  };
+                  sResult = remainHexa + sResult;
                }
-            } while (input2 != 0);
-            Console.WriteLine ($"The converted hexadecimal value of {input1} is {result}");
-         }
-         void Binary (int input2) {
+            }
+         } while (input2 != 0);
+         Console.WriteLine ($"The converted hexadecimal value of {sInput1} is {sResult}");
+      }
+
+      static void Binary (int input2) {
+         if (input2 == 0) sResult = "0";
+         else {
             do {
                int remain = input2 % 2;
                input2 /= 2;
-               result = remain.ToString () + result;
-            }
-            while (input2 != 0);
-            Console.WriteLine ($"The converted binary value of {input1} is {result}");
+               sResult = remain.ToString () + sResult;
+            } while (input2 != 0);
          }
+         Console.WriteLine ($"The converted binary value of {sInput1} is {sResult}");
       }
+
+      static int sInput1, sInput2;
+      static bool sIsValid;
+      static string sNumSystem = "", sResult = "";
    }
 }
