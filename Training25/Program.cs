9550a9f9171026e4443614a2298f72455e3f418e
Deepakhannan <deepakhannan.lakshmipathi@trumpf.com>
2025-09-24 10:58:42 +0530

    Second commit

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -8,29 +8,39 @@
 namespace Training25 {
    internal class Program {
       static void Main (string[] args) {
-         int result = 0;
-         int myNum1 = int.Parse (Console.ReadLine ());
-         int myNum2 = int.Parse (Console.ReadLine ());
-
-
-         int LcmNum (int myNum1, int myNum2) {
-            for (int i = 1; i <= myNum2; i++) {
-               int temp1 = myNum1 * i;
-               for (int j = 1; j <= myNum1; j++) {
-                  int temp2 = myNum2 * j;
-                  if (temp1 == temp2) {
-                     return temp1;
-                  }
-               }
-            }
-            return myNum1 * myNum2;
-         }
-         int Gcd(int myNum1, int myNum2) {
-            return (myNum1 * myNum2) / LcmNum (myNum1, myNum2);
-         }
-         Console.WriteLine (LcmNum(myNum1,myNum2));
-         Console.WriteLine (Gcd(myNum1,myNum2));
-
+         Console.Write ("Enter the first number: ");
+         do {
+            isValid = int.TryParse (Console.ReadLine (), out myNum1);
+            if (!isValid) Console.Write ("Please enter a valid number: ");
+         } while (!isValid);
+         Console.Write ("Enter the second number: ");
+         do {
+            isValid = int.TryParse (Console.ReadLine (), out myNum2);
+            if (!isValid) Console.Write ("Please enter a valid number: ");
+         } while (!isValid);
+         Console.WriteLine ("LCM: " + Lcm ());
+         if (myNum1 == 0 && myNum2 == 0) Console.WriteLine ("GCD: Does not exist !");
+         else Console.WriteLine ("GCD: " + Gcd ());
       }
+
+      static int Lcm () {
+         if (myNum1 == 0 || myNum2 == 0) return 0;
+         return (myNum1 * myNum2) / Gcd ();
+      }
+
+      static int Gcd () {
+         if (myNum1 == 0 || myNum2 == 0) return int.Max (myNum1, myNum2);
+         int gcd;
+         int temp1 = int.Max (myNum1, myNum2), temp2 = int.Min (myNum1, myNum2);
+         do {
+            gcd = temp1 % temp2;
+            temp1 = temp2;
+            temp2 = gcd;
+         } while (gcd != 0);
+         return temp1;
+      }
+
+      static int myNum1, myNum2;
+      static bool isValid;
    }
 }
