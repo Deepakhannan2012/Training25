091f3117c23e8cb1e6048ed67672f0edbe5b4333
Deepakhannan <deepakhannan.lakshmipathi@trumpf.com>
2025-09-30 11:16:23 +0530

    Reviewer changes 3

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -29,28 +29,22 @@ namespace Training25 {
       // Converts the number to word form
       // Handles from -999999999 to 999999999
       static string NumToWord () {
-         string[] ones = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
-         string[] tens = ["", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
-         string[] teens = ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen",
-                           "eighteen", "nineteen"];
+         if (sNum is 0) return "Zero";
+         if (sNum is < 0) { sNum = -sNum; return $"Minus {NumToWord ()}"; }
          Dictionary<int, string> numValue = new () { { 10000000, "crore" },{ 100000, "lakh" },{ 1000, "thousand" },
                                                      { 100, "hundred" },{ 1, "" } };
          var sb = new StringBuilder ();
-         if (sNum is 0) return "Zero";
-         if (sNum is < 0) {
-            sNum = -sNum;
-            return $"Minus {NumToWord ()}";
-         }
          foreach (var (num, word) in numValue) {
             int divNum = sNum / num;
-            if (divNum > 0) {
-               sb.Append ($"{TensToWord (divNum)} {word} ");
-               sNum %= num;
-            }
+            if (divNum > 0) { sb.Append ($"{TensToWord (divNum)} {word} "); sNum %= num; }
          }
          return sb.ToString ();
 
-         string TensToWord (int num) {
+         static string TensToWord (int num) {
+            string[] ones = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+            string[] tens = ["", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
+            string[] teens = ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen",
+                           "eighteen", "nineteen"];
             int divNum = num / 10, modNum = num % 10;
             return divNum switch {
                0 => ones[modNum],
@@ -63,16 +57,12 @@ namespace Training25 {
 
       // Converts the number to roman numerals
       static string NumToRoman () {
+         if (sNum is < 1 or > 3999) return "Does not exist !";
          Dictionary<int, string> numRoman = new () { { 1000, "M" },{ 900, "CM" },{ 500, "D" },{ 400, "CD" },
                                                      { 100, "C" },{ 90, "XC" },{ 50, "L" },{ 40, "IV" },{ 10, "X" },
                                                      { 9, "IX" },{ 5, "V" },{ 4, "IV" },{ 1, "I" } };
          var sb = new StringBuilder ();
-         if (sNum is < 1 or > 3999) return "Does not exist !";
-         foreach (var (num, roman) in numRoman)
-            while (sNum >= num) {
-               sb.Append (roman);
-               sNum -= num;
-            }
+         foreach (var (num, roman) in numRoman) while (sNum >= num) { sb.Append (roman); sNum -= num; }
          return sb.ToString ();
       }
 
