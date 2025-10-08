55bd0702d483651e72078c245dad5e3450141f92
Deepakhannan <deepakhannan.lakshmipathi@trumpf.com>
2025-10-08 13:50:07 +0530

    Review fix 3

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -13,7 +13,7 @@ internal class Program {
       sNum1 = GetNum ("first"); sNum2 = GetNum ("second");
       (int lcm, int gcd) = (sNum1, sNum2) switch {
          (0, 0) => (0, 0),
-         (_, 0) or (0, _) => (0, int.Max (Math.Abs (sNum1), Math.Abs (sNum2))),
+         (_, 0) or (0, _) => (0, int.Max (sNum1, sNum2)),
          _ => LCMAndGCD ()
       };
       Write ($"LCM: {lcm}\nGCD: {gcd}");
@@ -21,17 +21,17 @@ internal class Program {
 
    // Returns the LCM and GCD of two numbers
    static (int lcm, int gcd) LCMAndGCD () {
-      (int temp1, int temp2) = (Math.Abs (sNum1), Math.Abs (sNum2));
+      (int temp1, int temp2) = (sNum1, sNum2);
       while (temp2 is not 0) (temp1, temp2) = (temp2, temp1 % temp2);
-      return (Math.Abs (sNum1 * sNum2) / temp1, temp1);
+      return (sNum1 * sNum2 / temp1, temp1);
    }
 
    // Gets a valid integer input from the user
    static int GetNum (string count) {
       int num;
       Write ($"Enter the {count} number: ");
-      while (!int.TryParse (ReadLine (), out num)) WriteLine ("Enter a valid input !");
-      return num;
+      while (!int.TryParse (ReadLine (), out num)) Write ("Enter a valid input: ");
+      return Math.Abs (num);
    }
 
    static int sNum1, sNum2;
