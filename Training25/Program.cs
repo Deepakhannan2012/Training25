622277ae0dbb8f90f4e33fcf546ff5e528ded91b
Deepakhannan <deepakhannan.lakshmipathi@trumpf.com>
2025-10-09 11:58:37 +0530

    Initial commit

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -3,12 +3,22 @@
 // Copyright (c) Metamation India.
 // ------------------------------------------------------------------
 // Program.cs
-// Program on main branch.
+// Program on T06 branch.
 // ------------------------------------------------------------------------------------------------
-namespace Training25 {
-   internal class Program {
-      static void Main (string[] args) {
-         Console.WriteLine ("Hello, World!");
-      }
+using static System.Console;
+
+namespace Training25;
+internal class Program {
+   static void Main (string[] args) {
+      int num;
+      Write ("Enter a number to find its Digital Root: ");
+      while (!int.TryParse (ReadLine (), out num)) Write ("Enter a valid input: ");
+      Write ($"The digital root of {num} is {DigiRoot (num)}.");
+   }
+
+   // Returns the digital root of the input
+   static int DigiRoot (int num) {
+      int sum = (num > 9) ? num % 10 + DigiRoot (num / 10) : num;
+      return sum > 9 ? DigiRoot (sum) : sum;
    }
 }
