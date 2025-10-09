dfee31222a27e12516cb56b961c8ce333b8bef54
Deepakhannan <deepakhannan.lakshmipathi@trumpf.com>
2025-10-09 14:57:43 +0530

    Initial commit

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -3,12 +3,46 @@
 // Copyright (c) Metamation India.
 // ------------------------------------------------------------------
 // Program.cs
-// Program on main branch.
+// Program on T04 branch.
 // ------------------------------------------------------------------------------------------------
-namespace Training25 {
-   internal class Program {
-      static void Main (string[] args) {
-         Console.WriteLine ("Hello, World!");
+using static System.Console;
+using System.Text;
+
+namespace Training25;
+internal class Program {
+   static void Main (string[] args) {
+      OutputEncoding = new UnicodeEncoding ();
+      char[] white = ['♖', '♘', '♗', '♕', '♔', '♗', '♘', '♖'];
+      char[] black = ['♜', '♞', '♝', '♛', '♚', '♝', '♞', '♜'];
+      for (int i = 0; i < 33; i++) {
+         Action action = i switch {
+            0 => () => Border ('┌', '┬', '┐'),
+            2 => () => Pieces (black),
+            6 => () => Pawn ('♟'),
+            26 => () => Pawn ('♙'),
+            30 => () => Pieces (white),
+            32 => () => Border ('└', '┴', '┘'),
+            _ when i % 4 is 0 => () => Border ('├', '┼', '┤'),
+            _ => () => WriteLine ($"{String.Concat (Enumerable.Repeat ("│\t", 8))}│")
+         };
+         action ();
       }
    }
+
+   // Print pieces in a row
+   static void Pieces (char[] colour) {
+      for (int i = 0; i < 8; i++) Write ($"│   {colour[i]}   "); WriteLine ("│");
+   }
+
+   // Print border line
+   static void Border (char left, char mid, char right) {
+      WriteLine ($"{left}{String.Concat (Enumerable.Repeat ($"───────{mid}", 7))}───────{right}");
+   }
+
+   // Print a row of pawns
+   static void Pawn (char piece) {
+      WriteLine ($"{String.Concat (Enumerable.Repeat ($"│   {piece}   ", 8))}│");
+   }
 }
+
+
