7874bda1f4e48403ceefe1dff1ab660eafa46479
Deepakhannan <deepakhannan.lakshmipathi@trumpf.com>
2025-10-13 10:24:03 +0530

    Initial commit

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -3,12 +3,22 @@
 // Copyright (c) Metamation India.
 // ------------------------------------------------------------------
 // Program.cs
-// Program on main branch.
+// Program on T07 branch.
 // ------------------------------------------------------------------------------------------------
-namespace Training25 {
-   internal class Program {
-      static void Main (string[] args) {
-         Console.WriteLine ("Hello, World!");
+using static System.Console;
+
+namespace Training25;
+internal class Program {
+   static void Main (string[] args) {
+      int rowNum;
+      Write ("Enter the number of rows for the Pascal triangle: ");
+      while ((!int.TryParse (ReadLine (), out rowNum)) || rowNum < 1) Write ("Enter a valid input: ");
+      for (int i = 0; i < rowNum; i++) {
+         Write (new string (' ', rowNum - i));
+         for (int j = 0; j <= i; j++) Write ($"{Factorial (i) / (Factorial (j) * Factorial (i - j))} "); WriteLine ("");
       }
+
+      // Returns the factorial of the input parameter
+      static int Factorial (int num) => num is not 0 ? num * Factorial (num - 1) : 1;
    }
 }
