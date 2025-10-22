547b4518829d6df71e5308d8e4c5e4b453f57f8c
Deepakhannan <deepakhannan.lakshmipathi@trumpf.com>
2025-10-22 15:37:46 +0530

    Initial commit

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -3,12 +3,30 @@
 // Copyright (c) Metamation India.
 // ------------------------------------------------------------------
 // Program.cs
-// Program on main branch.
+// Program on T09 branch.
 // ------------------------------------------------------------------------------------------------
-namespace Training25 {
-   internal class Program {
-      static void Main (string[] args) {
-         Console.WriteLine ("Hello, World!");
-      }
+using System.Text;
+using static System.Console;
+
+namespace Training25;
+internal class Program {
+   static void Main (string[] args) {
+      Write ("Enter a string to reduce: ");
+      sInput = ReadLine () ?? "";
+      string output = ReducedString().Length is 0 ? "an empty string" : ReducedString ();
+      WriteLine ($"The reduced form is {output}.");
    }
+
+   // Returns the reduced form of the input string
+   static string ReducedString () {
+      StringBuilder sb = new ();
+      char[] myArray = sInput.ToLower().ToCharArray ();
+      for (int i = 0; i < myArray.Length;) {
+         if (i < myArray.Length - 1 && myArray[i] == myArray[i + 1]) i += 2;
+         else { sb.Append (myArray[i]); i++; }
+      }
+      return sb.ToString ();
+   }
+
+   static string sInput = "";
 }
