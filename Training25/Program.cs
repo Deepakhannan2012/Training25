ef148d54f77e9d7d0c5cde8ad52b574fcbe266a9
Deepakhannan <deepakhannan.lakshmipathi@trumpf.com>
2025-10-27 11:27:21 +0530

    Initial commit

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -3,12 +3,41 @@
 // Copyright (c) Metamation India.
 // ------------------------------------------------------------------
 // Program.cs
-// Program on main branch.
+// Program on T11.1 branch.
 // ------------------------------------------------------------------------------------------------
-namespace Training25 {
-   internal class Program {
-      static void Main (string[] args) {
-         Console.WriteLine ("Hello, World!");
+using static System.Console;
+
+namespace Training25;
+
+internal class Program {
+   static void Main () {
+      int num;
+      // First 25 Armstrong numbers are handled as it takes longer to compute beyond the 25 numbers
+      Write ("Enter a number(n) between 1 and 25 to find the nth Armstrong number: ");
+      while (!int.TryParse (ReadLine (), out num) || num > 25) Write ("Enter a valid input: ");
+      (int counter, int firstNum) = (0, 0);
+      while (true) {
+         if (IsArmstrong (firstNum++) && ++counter == num) {
+            string suffix = (num % 10) switch {
+               1 when num is not 11 => "st",
+               2 when num is not 12 => "nd",
+               3 when num is not 13 => "rd",
+               _ => "th"
+            };
+            WriteLine ($"The {num}{suffix} Armstrong number is {firstNum - 1}");
+            break;
+         }
       }
+   }
+
+   // Checks if a number is an Armstrong number
+   static bool IsArmstrong (int num) {
+      if (num < 10) return true;
+      int digits = (int)Math.Log10 (num) + 1;
+      return num == Total (num, digits);
+
+      // Calculate the sum of the digits raised to the power of number of digits
+      static int Total (int num, int digits)
+         => (num is not 0) ? (int)Math.Pow (num % 10, digits) + Total (num / 10, digits) : 0;
    }
 }
