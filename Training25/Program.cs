d896b096a29f30faeaa2cf4a64157d4a05dc92a5
Deepakhannan <deepakhannan.lakshmipathi@trumpf.com>
2025-10-27 14:08:57 +0530

    Initial commit

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -3,12 +3,32 @@
 // Copyright (c) Metamation India.
 // ------------------------------------------------------------------
 // Program.cs
-// Program on main branch.
+// Program on T12 branch.
 // ------------------------------------------------------------------------------------------------
-namespace Training25 {
-   internal class Program {
-      static void Main (string[] args) {
-         Console.WriteLine ("Hello, World!");
-      }
+using static System.Console;
+
+namespace Training25;
+
+internal class Program {
+   static void Main () {
+      string input;
+      bool isValid;
+      Write ("Enter the string representation of the votes: ");
+      do {
+         input = ReadLine () ?? "";
+         isValid = !string.IsNullOrEmpty (input) && input.Trim ().All (Char.IsLetter);
+         if (!isValid) Write ("Invalid input. Please enter a string containing only letters: ");
+      } while (!isValid);
+      MaxVotes (input, out char maxChar, out int maxVote);
+      WriteLine ($"The winner is {maxChar} with {maxVote} votes");
+   }
+
+   // Finds the letter with the maximum votes and its count
+   static void MaxVotes (string input, out char maxChar, out int maxVotes) {
+      Dictionary<char, int> count = [];
+      foreach (char ch in input.ToLower ())
+         count[ch] = count.ContainsKey (ch) ? ++count[ch] : 1;
+      maxVotes = count.Values.Max ();
+      maxChar = count.Aggregate ((a, b) => a.Value >= b.Value ? a : b).Key;
    }
 }
