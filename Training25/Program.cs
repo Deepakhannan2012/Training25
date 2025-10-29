5e106e6a97a900eec5b18deeba11dfc3f486bc23
Deepakhannan <deepakhannan.lakshmipathi@trumpf.com>
2025-10-29 10:47:39 +0530

    Initial commit

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -3,12 +3,31 @@
 // Copyright (c) Metamation India.
 // ------------------------------------------------------------------
 // Program.cs
-// Program on main branch.
+// Program on Test1 branch.
 // ------------------------------------------------------------------------------------------------
-namespace Training25 {
-   internal class Program {
-      static void Main (string[] args) {
-         Console.WriteLine ("Hello, World!");
-      }
+using static System.Console;
+
+namespace Training25;
+
+internal class Program {
+   static void Main () {
+      Write ("Enter the number the separate the even and odd digits: ");
+      while (!int.TryParse (ReadLine (), out num) || num < 1) Write ("Enter a valid input: ");
+      WriteLine ($"The sorted number is: {DigitSorter ()}");
    }
+
+   static string DigitSorter () {
+      int[] numArray = new int[(int)Math.Log10 (num) + 1];
+      (int startIndex, int endIndex) = (0, 0);
+      while (num > 0) {
+         int digit = num % 10;
+         numArray[(digit % 2 is 0) ? startIndex++ : ^++endIndex] = digit;
+         num /= 10;
+      }
+      Array.Sort (numArray, 0, startIndex);
+      Array.Sort (numArray, numArray.Length - endIndex, endIndex);
+      return string.Join ("", numArray);
+   }
+
+   static int num;
 }
