f80ec83ab911c9ef2252758fc01cfde05abb66c5
Deepakhannan <deepakhannan.lakshmipathi@trumpf.com>
2025-11-04 10:48:06 +0530

    Initial commit

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -3,12 +3,36 @@
 // Copyright (c) Metamation India.
 // ------------------------------------------------------------------
 // Program.cs
-// Program on main branch.
+// Program on T14 branch.
 // ------------------------------------------------------------------------------------------------
-namespace Training25 {
-   internal class Program {
-      static void Main (string[] args) {
-         Console.WriteLine ("Hello, World!");
-      }
+using static System.Console;
+
+namespace Training25;
+
+internal class Program {
+   static void Main (string[] args) {
+      Write ("Enter a positive number to find its smallest transform: ");
+      while (!int.TryParse (ReadLine (), out sInput) || sInput < 1) Write ("Invalid input ! Enter a valid number: ");
+      WriteLine ($"The smallest number of changes required is {SmallTransform ()}.");
    }
+
+   // Returns the smallest transform
+   static int SmallTransform () {
+      int[] numArray = new int[sInput.ToString ().Length];
+      int index = 0;
+      while (sInput is not 0) {
+         numArray[index++] = sInput % 10;
+         sInput /= 10;
+      }
+      int minValue = int.MaxValue;
+      foreach (var item in numArray) {
+         int temp = 0;
+         foreach (var item1 in numArray)
+            temp += Math.Abs (item1 - item);
+         if (temp < minValue) minValue = temp;
+      }
+      return minValue;
+   }
+
+   static int sInput;
 }
